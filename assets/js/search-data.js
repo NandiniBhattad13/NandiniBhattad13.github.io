cdeb74c147af454c87ed6ea97956b97ca52c8f17
NandiniBhattad13 <141108583+NandiniBhattad13@users.noreply.github.com>
2025-11-25 14:02:36 +0000

    Deploying to gh-pages from @ NandiniBhattad13/NandiniBhattad13.github.io@60fa2cac5adab33093a8d442e29b6da4ad9b39fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,9 +422,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-sentiment-analysis",
+          title: 'Sentiment Analysis',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
