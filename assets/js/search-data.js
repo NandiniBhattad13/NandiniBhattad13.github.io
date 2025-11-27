0cae7786f4897366eb6ac9009acd27c43d0f0ae0
NandiniBhattad13 <141108583+NandiniBhattad13@users.noreply.github.com>
2025-11-27 17:10:55 +0000

    Deploying to gh-pages from @ NandiniBhattad13/NandiniBhattad13.github.io@b7d5a0e46d5a63829ef33c0c579066c763cf5553 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Here are some projects on machine learning and statistics that I have worked on!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
